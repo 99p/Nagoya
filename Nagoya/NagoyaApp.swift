b867628713209183b66111bb54fa80f49949d5af
99p <poccori@gmail.com>
2022-07-11 23:49:37 +0900

    Initial Commit

---
+++ Nagoya/NagoyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NagoyaApp.swift
+//  Nagoya
+//
+//  Created by macboy on 2022/07/11.
+//
+
+import SwiftUI
+
+@main
+struct NagoyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
