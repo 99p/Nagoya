dc7a5d7b480d6d32bbf17d1f170dd39b3bdf130a
99p <poccori@gmail.com>
2022-07-16 09:33:20 +0900

    add touch move

--- Nagoya/ContentView.swift
+++ Nagoya/ContentView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 import SpriteKit //1
 
-let bgColor = UIColor.gray
-
 class GameScene : SKScene{
     
+    var bowl:SKSpriteNode?
+    
     override func didMove(to view: SKView) {
+        
+        self.physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
         
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(
@@ -21,7 +23,19 @@ class GameScene : SKScene{
         background.size = self.size
         self.addChild(background)
         
+        let bowlTexture = SKTexture(imageNamed: "bowl")
+        let bowl = SKSpriteNode(texture: bowlTexture)
+        bowl.position = CGPoint(x: self.size.width*0.5, y: 100)
+        bowl.size = CGSize(
+            width: bowlTexture.size().width*0.5,
+            height: bowlTexture.size().height*0.5)
+        bowl.physicsBody = SKPhysicsBody(texture: bowlTexture, size: bowl.size)
+        bowl.physicsBody?.isDynamic = false
+        self.bowl = bowl
+        self.addChild(bowl)
+        
         fallNagoyaSpecialty()
+        
     }
     
     func fallNagoyaSpecialty(){
@@ -36,7 +50,28 @@ class GameScene : SKScene{
             width: texture.size().width*0.5,
             height: texture.size().height*0.5)
         
+        sprite.physicsBody = SKPhysicsBody(texture: texture, size: sprite.size)
+        
         self.addChild(sprite)
+        
+    }
+    
+    // タッチ開始時
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch: AnyObject = touches.first {
+            let location = touch.location(in: self)
+            let action = SKAction.move(to: CGPoint(x: location.x, y: 100), duration: 0.1)
+            self.bowl?.run(action)
+        }
+    }
+    
+    // ドラッグ時
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            let action = SKAction.move(to: CGPoint(x: location.x, y: 100), duration: 0.1)
+            self.bowl?.run(action)
+        }
     }
     
 }
@@ -51,9 +86,8 @@ struct ContentView: View {
     
     var body: some View {
         ZStack{
-            Color(bgColor)
-                .ignoresSafeArea()
             SpriteView(scene: scene, debugOptions: [.showsFPS, .showsDrawCount])
+//                .edgesIgnoringSafeArea(.all)
         }
     }
 }
