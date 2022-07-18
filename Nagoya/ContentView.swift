981f39c75dfb4bcced314fb373dcbaf65bb874ca
99p <poccori@gmail.com>
2022-07-18 13:53:08 +0900

    done Nagoya

--- Nagoya/ContentView.swift
+++ Nagoya/ContentView.swift
@@ -8,13 +8,26 @@
 import SwiftUI
 import SpriteKit //1
 
-class GameScene : SKScene{
+class GameScene : SKScene, SKPhysicsContactDelegate{
     
+    // どんぶり
     var bowl:SKSpriteNode?
+    
+    // タイマー
+    var timer: Timer?
+    
+    // 落下判定用シェイプ
+    var lowestShape: SKShapeNode?
+    
+    // スコア用プロパティ
+    var score = 0
+    var scoreLabel: SKLabelNode?
+    var scoreList = [100, 200, 300, 500, 800, 1000, 1500]
     
     override func didMove(to view: SKView) {
         
         self.physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
+        self.physicsWorld.contactDelegate = self
         
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(
@@ -22,6 +35,19 @@ class GameScene : SKScene{
             y: self.size.height*0.5)
         background.size = self.size
         self.addChild(background)
+        
+        // 落下検知用シェイプノードを追加
+        let lowestShape = SKShapeNode(rectOf: CGSize(width: self.size.width*3, height: 10))
+        lowestShape.position = CGPoint(x: self.size.width*0.5, y: -10)
+//        lowestShape.fillColor = .blue
+        
+        // シェイプに合わせてPhysicsBodyを生成
+        let physicsBody = SKPhysicsBody(rectangleOf: lowestShape.frame.size)
+        physicsBody.isDynamic = false
+        physicsBody.contactTestBitMask = 0x1 << 1 //衝突を検知する対象のビットマスクを指定
+        lowestShape.physicsBody = physicsBody
+        self.addChild(lowestShape)
+        self.lowestShape = lowestShape
         
         let bowlTexture = SKTexture(imageNamed: "bowl")
         let bowl = SKSpriteNode(texture: bowlTexture)
@@ -34,26 +60,48 @@ class GameScene : SKScene{
         self.bowl = bowl
         self.addChild(bowl)
         
-        fallNagoyaSpecialty()
+        // スコア用ラベル
+        var scoreLabel = SKLabelNode(fontNamed: "Helvetica")
+        scoreLabel.position = CGPoint(x: self.size.width*0.92, y: self.size.height*0.78)
+        scoreLabel.text = "￥0"
+        scoreLabel.fontSize = 32
+        scoreLabel.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.right
+        scoreLabel.fontColor = UIColor.green
+        self.addChild(scoreLabel)
+        self.scoreLabel = scoreLabel
+        
+        self.fallNagoyaSpecialty()
+        
+        // タイマーを生成
+        self.timer = Timer.scheduledTimer(timeInterval: 3,
+                                          target: self,
+                                          selector: #selector(fallNagoyaSpecialty),
+                                          userInfo: nil,
+                                          repeats: true)
         
     }
     
-    func fallNagoyaSpecialty(){
+    
+    @objc func fallNagoyaSpecialty(){
         
-        let texture = SKTexture(imageNamed: "0")
+        let index = Int(arc4random_uniform(7))
+        let texture = SKTexture(imageNamed: "\(index)")
         let sprite = SKSpriteNode(texture: texture)
         
         sprite.position = CGPoint(
             x: self.size.width*0.5,
-            y: self.size.height*0.5)
+            y: self.size.height)
         sprite.size = CGSize(
             width: texture.size().width*0.5,
             height: texture.size().height*0.5)
         
         sprite.physicsBody = SKPhysicsBody(texture: texture, size: sprite.size)
+        sprite.physicsBody?.contactTestBitMask = 0x1 << 1 //衝突を検知する対象のビットマスクを指定
         
         self.addChild(sprite)
         
+        self.score += self.scoreList[index]
+        self.scoreLabel?.text = "￥\(self.score)"
     }
     
     // タッチ開始時
@@ -71,6 +119,17 @@ class GameScene : SKScene{
             let location = touch.location(in: self)
             let action = SKAction.move(to: CGPoint(x: location.x, y: 100), duration: 0.1)
             self.bowl?.run(action)
+        }
+    }
+    
+    // GameOver
+    func didBegin(_ contact: SKPhysicsContact) {
+        if contact.bodyA.node == self.lowestShape || contact.bodyB.node == self.lowestShape {
+            let sprite = SKSpriteNode(imageNamed: "gameover")
+            sprite.position = CGPoint(x: self.size.width*0.5, y: self.size.height*0.5)
+            self.addChild(sprite)
+            self.isPaused = true
+            self.timer?.invalidate()
         }
     }
     
